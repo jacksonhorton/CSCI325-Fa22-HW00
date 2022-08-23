b44810fc6a53885d1ac7f8982d9786f7ea95d8d6
Jackson Horton <jacrhort@cs1-test.utm.edu>
2022-08-23 15:06:52 -0500

    Test script created

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int MAX_ARRAY_SIZE = 100;
+  int int_array[MAX_ARRAY_SIZE];
+
+  int i = 0;
+  while (i<99) {
+    cout << i << " ";
+    i++;
+  }
+  return 0;
+}
