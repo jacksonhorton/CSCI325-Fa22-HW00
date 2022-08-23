17542e3a84b2ae191c3b6259540a838124ef6908
Jackson Horton <jacrhort@cs1-test.utm.edu>
2022-08-23 15:19:33 -0500

    Update

--- main.cpp
+++ main.cpp
@@ -7,9 +7,15 @@ int main() {
   int int_array[MAX_ARRAY_SIZE];
 
   int i = 0;
-  while (i<99) {
-    cout << i << " ";
+  while (i<=99) {	     
+    int_array[i] = i;	       	
+    //cout << std::to_string(i) << " ";
     i++;
   }
+  
+  cout << endl;
+  for (int n: int_array) {
+    cout << n << " ";
+  };
   return 0;
 }
